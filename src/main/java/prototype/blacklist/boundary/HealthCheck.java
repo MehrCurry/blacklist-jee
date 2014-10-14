72ab8bfa647b3877df7565b57d5277d42af2df1a
AN <AN@AN-PC>
2014-10-14 18:01:03 +0200

    * removed unused imports  * removed template generated text  * set UTF-8 as encoding

--- src/main/java/prototype/blacklist/boundary/HealthCheck.java
+++ src/main/java/prototype/blacklist/boundary/HealthCheck.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prototype.blacklist.boundary;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Date;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
--- src/main/java/prototype/blacklist/config/RESTConfiguration.java
+++ src/main/java/prototype/blacklist/config/RESTConfiguration.java
@@ -1,14 +1,8 @@
 package prototype.blacklist.config;
 
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Guido.Zockoll
