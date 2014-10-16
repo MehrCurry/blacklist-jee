c91214a74057844514e7af6d7541c46637d01fb8
Jan Beye <jan.beye@payone.de>
2014-10-16 15:48:13 +0200

    small refactorings

--- src/main/java/prototype/blacklist/presentation/Index.java
+++ src/main/java/prototype/blacklist/presentation/Index.java
@@ -1,22 +1,21 @@
 package prototype.blacklist.presentation;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.type.TypeFactory;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.json.JsonArray;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import javax.faces.FacesException;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 
 /**
  * Created by jbeye on 15.10.14.
@@ -25,8 +24,8 @@ import java.util.List;
 @RequestScoped
 public class Index {
 
-    private List<String> blacklistUrls;
-
+    private List<String> blacklistUrls;       
+    
     private Client client;
 
     private WebTarget target;
@@ -51,19 +50,16 @@ public class Index {
                     ext.getRequestContextPath(), null, null);
             return uri.toASCIIString();
         } catch (URISyntaxException e) {
-            return "http://localhost:8080/blacklist-jee7/";
+            throw new FacesException(e);
         }
     }
 
     @PostConstruct
     public void init(){
-        this.client = ClientBuilder.newClient();
+        this.client = ClientBuilder.newBuilder().register(JacksonJsonProvider.class).build();
         this.target = this.client.target(getApplicationUri() + "/resources/blacklist");
-        JsonArray response = this.target.request().get(JsonArray.class);
-        blacklistUrls = new ArrayList<>();
-        for (int i=0; i<response.size(); i++) {
-            blacklistUrls.add(response.getString(i) );
-        }
+        Collection<String> list = this.target.request().get(Collection.class);
+        this.blacklistUrls = new ArrayList<>(list);        
     }
 
     public void delete(String url){
