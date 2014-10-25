1ef7eff1df88143bb606a1db1f217a0d912af59b
patStar <dergeigerzaehler-hongkong@yahoo.de>
2014-10-25 12:40:09 +0200

    removed duplicated code

--- src/main/java/prototype/blacklist/normalize/BlacklistCheckValueNormalizer.java
+++ src/main/java/prototype/blacklist/normalize/BlacklistCheckValueNormalizer.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import prototype.blacklist.boundary.BlacklistCheck;
 import prototype.blacklist.entity.BlacklistEntry;
@@ -14,21 +15,15 @@ import prototype.blacklist.entity.BlacklistEntry;
 @Stateless
 public class BlacklistCheckValueNormalizer {
     
+	@Inject
+	BlacklistValueNormalizer normalizer;
+	
     public void normalize(final BlacklistCheck check) {
     	Map<String, String> parametersToCheck = check.getParametersToCheck();
         for(Entry<String,String> checkEnty : parametersToCheck.entrySet()){
-        	Normalizer normalizer = createNormalizerByName(checkEnty.getKey());
-        	String value = normalizer.normalize(checkEnty.getValue());
+        	Normalizer specificNormalzer = normalizer.createNormalizerByName(checkEnty.getKey());
+        	String value = specificNormalzer.normalize(checkEnty.getValue());
         	parametersToCheck.put(checkEnty.getKey(), value);
-        }
-    }
-    
-    public Normalizer createNormalizerByName(final String name) {
-        switch (name) {
-            case "iban":
-                    return new IBANNormalizer();
-            default:
-                    return new TrimNormalizer();
         }
     }
 }
