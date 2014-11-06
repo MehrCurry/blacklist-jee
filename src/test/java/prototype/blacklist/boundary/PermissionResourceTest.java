c3ba66fd7559c767ee6c88a29185c42eca71f880
Guido Zockoll <gzockoll@gmail.com>
2014-11-06 16:33:24 +0100

    cleanup tests fixed

--- src/test/java/prototype/blacklist/boundary/PermissionResourceTest.java
+++ src/test/java/prototype/blacklist/boundary/PermissionResourceTest.java
@@ -7,17 +7,17 @@ package prototype.blacklist.boundary;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Matchers;
-import org.mockito.Mockito;
+import prototype.blacklist.control.BlacklistRepository;
 import prototype.blacklist.entity.Blacklist;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 public class PermissionResourceTest {
     private PermissionResource cut;
@@ -31,36 +31,40 @@ public class PermissionResourceTest {
     public void tearDown() {
     }
 
-    private EntityManager createEntityManagerMock(List result) {
-        Query query=Mockito.mock(Query.class);
-        Mockito.when(query.getResultList()).thenReturn(result);
-        EntityManager em=Mockito.mock(EntityManager.class);
-        Mockito.when(em.createNamedQuery(Matchers.anyString())).thenReturn(query);
-        return em;
-    }
-
     @Test
-    @Ignore
     public void an_unknown_blacklist_should_give_a_bad_request_response() {
-        EntityManager em=createEntityManagerMock(Collections.EMPTY_LIST);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
 
-        // assertThat(cut.isGranted("foo", "bar").getStatus()).isEqualTo(Status.BAD_REQUEST.getStatusCode());
+        cut.setRepository(createRepositoryMock(Collections.EMPTY_LIST));
+
+        cut.isGranted("foo", "bar", resp);
+        verify(resp).setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    private BlacklistRepository createRepositoryMock(List results) {
+        BlacklistRepository repository = mock(BlacklistRepository.class);
+        when(repository.findByName(Matchers.anyString())).thenReturn(results);
+        return repository;
     }
 
     @Test
-    @Ignore
     public void a_known_blacklist_with_an_matching_entry_should_result_in_a_forbidden_reponse() {
-        EntityManager em = createEntityManagerMock(Arrays.asList(new Blacklist("foo").addEntry("bar")));
+        HttpServletResponse resp = mock(HttpServletResponse.class);
 
-        //assertThat(cut.isGranted("foo", "bar").getStatus()).isEqualTo(Status.FORBIDDEN.getStatusCode());
+        cut.setRepository(createRepositoryMock(Arrays.asList(new Blacklist("foo").addEntry("bar"))));
+
+        cut.isGranted("foo", "bar", resp);
+        verify(resp).setStatus(HttpServletResponse.SC_FORBIDDEN);
     }
 
     @Test
-    @Ignore
     public void a_known_blacklist_with_an_non_matching_entry_should_result_in_a_empty_result() {
-        EntityManager em = createEntityManagerMock(Arrays.asList(new Blacklist("foo").addEntry("baz")));
+        HttpServletResponse resp = mock(HttpServletResponse.class);
 
-        // assertThat(cut.isGranted("foo", "bar").getStatus()).isEqualTo(Status.NO_CONTENT.getStatusCode());
+        cut.setRepository(createRepositoryMock(Arrays.asList(new Blacklist("foo").addEntry("baz"))));
+
+        cut.isGranted("foo", "bar", resp);
+        verify(resp).setStatus(HttpServletResponse.SC_NO_CONTENT);
     }
 
 }
